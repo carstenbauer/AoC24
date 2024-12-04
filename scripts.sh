17bb39805773d23dabf9aab9115f3d229e9c464d
Carsten Bauer <crstnbr@gmail.com>
2024-12-04 20:26:51 +0100

    Update scripts.sh

--- scripts.sh
+++ scripts.sh
@@ -1,4 +1,4 @@
-AOC_COOKIE="53616c7465645f5ff564a3e39b9abc58f190bee85c89497102310ab08fca8f6d8879c7cb552487673da6f8733e3fb5859d14619780f216073ab912cfbced5545" # get this from the cookies tab in network tools on the AOC website
+AOC_COOKIE="" # get this from the cookies tab in network tools on the AOC website
 
 alias aos="python3 solution.py < in.txt"
 alias aot="echo -ne '\\e[0;34m'; python3 solution.py < test.txt; echo -ne '\\e[0m'"
